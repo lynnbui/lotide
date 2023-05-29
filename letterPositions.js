8e6911b51cf74d995db5b5d03c73742d990a32d2
Lynn <41266715+lynnbui@users.noreply.github.com>
2023-05-28 23:32:22 -0400

    Add test cases

--- letterPositions.js
+++ letterPositions.js
@@ -29,4 +29,8 @@ const letterPositions = (sentence) => {
     return results;
   };
   
-  console.log(letterPositions('hello'));
+console.log(letterPositions('hello'));
+assertArraysEqual(letterPositions('hello').e, [1])
+assertArraysEqual(letterPositions('hello').h, [0])
+assertArraysEqual(letterPositions('hello').l, [2, 3])
+assertArraysEqual(letterPositions('hello').o, [4])
