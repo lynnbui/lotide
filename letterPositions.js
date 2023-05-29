a0fbf2a8addde15cd93f8cd153a7365244d06732
Lynn <41266715+lynnbui@users.noreply.github.com>
2023-05-28 23:30:01 -0400

    Initial commit

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const eqArrays = (actual, expected) => {
+    if (actual.length !== expected.length) return false;
+  
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) return false;
+    }
+    return true;
+};
+  
+const assertArraysEqual = (actual, expected) => {
+    if (eqArrays(actual, expected)) return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const letterPositions = (sentence) => {
+    const results = {};
+    for (let i = 0; i < sentence.length; i++) {
+      if (sentence[i] === ' ') {
+        continue;
+      }
+  
+      if (sentence[i] in results) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+    return results;
+  };
+  
+  console.log(letterPositions('hello'));
