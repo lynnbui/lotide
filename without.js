008e80cd2b026617ba15ccd6d5dd112a4eb44604
Lynn <41266715+lynnbui@users.noreply.github.com>
2023-05-28 22:19:46 -0400

    Initial commit

---
+++ without.js
@@ -0,0 +1,28 @@
+const eqArrays = (actual, expected) => {
+    if (actual.length !== expected.length) return false;
+  
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) return false;
+    }
+    return true;
+  };
+  
+  const assertArraysEqual = (actual, expected) => {
+    if (eqArrays(actual, expected)) return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  };
+  
+  const without = (source, itemsToRemove) => {
+    let output = [];
+    for (const item of source) {
+      if (!itemsToRemove.includes(item)) {
+        output.push(item);
+      }
+    }
+    return output;
+  };
+  
+  const words = ["hello", "world", "lighthouse"];
+  without(words, ["lighthouse"]);
+  assertArraysEqual(words, ["hello", "world", "lighthouse"]);
